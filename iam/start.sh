3b7336171d3b3add313918eca73c2f244c796ae0
Justin Joco <justin.joco@gmail.com>
2024-08-31 13:53:24 -0700

    creates new resource server

--- iam/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export OIDC_RSA_PRIVATE_KEY=$(cat oidc.key)
-python manage.py runserver
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd iam
+export OIDC_RSA_PRIVATE_KEY=$(cat oidc.key)
+python manage.py runserver 8080 &
+cd ..
+cd resource_server
+python manage.py runserver 8081
